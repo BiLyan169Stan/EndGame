709af8df99f01ebf505a07359a5a3fa61c4e79c6
brian <brian@ADIEL>
2025-04-27 21:17:16 -0600

    Simulacion de EndGame

---
+++ src/main/java/com/edu/netbeans/endgame/EndGame.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edu.netbeans.endgame;
+
+/**
+ *
+ * @author brian
+ */
+public class EndGame{
+
+    public static void main(String[] args) {
+        Heroe blackPanther = new Heroe("Black Panter grr", "Ser el rey de todo Wakanda", "Garras de Vibranium");
+        Villano galactus = new Villano("Galactus el devorador de mundos", "Devorar mundos", "usa de chalan a Silver Surfer");
+        Avenger ironMan = new Avenger("Tony stark", "Genio" ,"Filantropo, multimillonario, playboy", "Mark 42", "Prime");
+        Thanos thanos = new Thanos("Thanos", "El Titan loco", "ejercito Chitaury", "Gemas del infinito");
+        
+        //Aplicando Polimorfismo
+        Personaje[] personajes = {blackPanther, galactus, ironMan, thanos};
+        for(Personaje personaje : personajes){
+            personaje.atacar();
+        }
+        
+        //Método especifico de Avenger
+        ((Avenger)ironMan).reunirse();
+        
+        //Ejemplo metodo especifico de Thanos
+        thanos.chasquearDedos();
+    }
+}
