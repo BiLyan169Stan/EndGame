b4a768d26410a8cf9e6108c9bcf5fe5d4760049b
brian <brian@ADIEL>
2025-04-27 21:19:40 -0600

    Simulacion de EndGame

---
+++ src/main/java/com/edu/netbeans/endgame/Avenger.java
@@ -0,0 +1,24 @@
+
+import com.edu.netbeans.endgame.Heroe;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author brian
+ */
+public class Avenger extends Heroe{
+    protected String equipo;
+    
+    public Avenger(String nombre, String poder, String arma, String equipo){
+        super(nombre, poder, arma);
+        this.equipo = equipo;
+    }
+     
+    public void reunirse(){
+        System.out.println(nombre+"se une al equipo"+equipo+"de los avengers");
+    }    
+}
